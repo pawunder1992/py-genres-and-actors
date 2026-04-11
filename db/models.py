102a928bee1b297eb29aaf35760a2de7700f126e
pawunder1992 <pavelkryvutsa@gmail.com>
2026-04-12 00:09:32 +0300

    solved issues with flake8

--- db/models.py
+++ db/models.py
@@ -5,8 +5,6 @@ class Genre(models.Model):
     name = models.CharField(max_length=255)
 
 
-
-
 class Actor(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
